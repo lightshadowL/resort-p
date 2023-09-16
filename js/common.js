28e0d4d958131f4563fd20866b53128a64e23754
lsljw65 <lsljw94@daum.net>
2023-09-16 15:18:45 +0900

    모바일 네비 동작

--- js/common.js
+++ js/common.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
-//  jqeury 시작위치
-  
+    //  jqeury 시작위치
+    /* ***************************************************** */
+    //모바일 네비 drop
+    $(".side-list>li").click(function(){
+        if($(this).find(".m-drop-list").is(":hidden")){
+            $(".m-drop-list").slideUp();
+            $("span i").addClass("fa-angle-right")
+            $(this).find("span i").removeClass("fa-angle-right")
+            $(this).find("span i").addClass("fa-angle-down")
+            $(this).find(".m-drop-list").slideDown();
+        }else{
+            $(this).find(".m-drop-list").slideUp();
+            $(this).find("span i").removeClass("fa-angle-down")
+            $(this).find("span i").addClass("fa-angle-right")
+        }
+    })
+    /* ********************************************************** */
+    //모바일 open side
+    $(".open-side").click(function(){
+        $(".side-content").addClass("active")
+    })
+    /* ********************************************************** */
+    //모바일 close side
+    $(".close-side").click(function(){
+        $(".side-content").removeClass("active")
+        $(".m-drop-list").slideUp();
+        $(".side-list>li span i").removeClass("fa-angle-down");
+        $(".side-list>li span i").addClass("fa-angle-right");
+    })
+
 })
