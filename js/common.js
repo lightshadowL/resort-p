b37ea9eda623f2f76d79ca3c1eb30edbca8cdc0d
lsljw65 <lsljw94@daum.net>
2023-09-17 14:22:55 +0900

    데스크탑 네비게이션 jquery

--- js/common.js
+++ js/common.js
@@ -29,4 +29,47 @@ $(document).ready(function(){
         $(".side-list>li span i").addClass("fa-angle-right");
     })
 
+    // 데스크탑 드롭
+    $(".desk-list>ul>li").each(function(index){
+        $(this).mouseenter(function(){
+            $(".desk-full-drop").stop().slideDown();
+            console.log("li에 있습니다.")
+            $(this).prev().find("a").removeClass("active");
+            $(this).next().find("a").removeClass("active");
+            $(this).find("a").addClass("active");
+            $(".full-drop-list>div").eq(index).prev().removeClass("active")
+            $(".full-drop-list>div").eq(index).next().removeClass("active")
+
+            if($(".full-drop-list>div").eq(index).find("a").length!==0){    
+                $(".full-drop-list>div").eq(index).addClass("active")
+            }
+            
+        }) 
+    })
+    $(".full-drop-list>div").each(function(index){
+        $(this).hover(
+            function(){
+                // console.log("길이 : "+$(this).find("a").length)
+                if($(this).find("a").length!==0){
+                    $(this).addClass("active");
+                }
+                
+                $(".desk-list>ul>li").eq(index).find("a").addClass("active")
+            },
+            function(){
+                $(".full-drop-list>div").removeClass("active");
+                $(".desk-list>ul>li").find("a").removeClass("active");
+            }
+        )
+    })
+
+    $(".desk-list").parent().mouseleave(function(){
+        $(".desk-full-drop").stop().slideUp();
+        $(".desk-list>ul>li").find("a").removeClass("active")
+        console.log("desk-bottom에 있습니다.")
+    })
+    
+        
+    
+
 })
